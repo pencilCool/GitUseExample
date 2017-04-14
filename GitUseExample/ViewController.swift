a6a41e267bdd599cf70db195cbb68ec996ca7951
pencilCool <yhtang_xt@163.com>
2017-04-14 09:36:59 +0800

    print("You finished!")

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+       print("You finished!")
     }
 
     
