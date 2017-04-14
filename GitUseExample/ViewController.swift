a3f583fd6c55adb423fc5febd24874bd51918b98
pencilCool <yhtang_xt@163.com>
2017-04-14 09:15:06 +0800

    1

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
-
+    
 }
 
